2bf8d2bb5b7a93b330e771bdbac1035da3e444ac
Kilavagora <kilavagora@users.noreply.github.com>
2017-07-31 23:19:01 -0500

    Add env port for heroku.

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const app = express();
-
-//app.get('/', function (req, res) {
-// res.send('Hello World!');
-//});
+let port = process.env.PORT || 8080;
 
 app.use('/', express.static('static'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + "!");
 });
 
