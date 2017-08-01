7a49c5825a3690c1b7347e1e81317d79b14e5c40
Kilavagora <kilavagora@users.noreply.github.com>
2017-07-31 22:35:19 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+Demo App
+========
+A barebones Node.js static file server app using Express 4.
+
+
---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+//app.get('/', function (req, res) {
+// res.send('Hello World!');
+//});
+
+app.use('/', express.static('static'));
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
